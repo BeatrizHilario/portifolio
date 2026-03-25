0bcf3c8f59c4e92b966a10618c4517452bf81b37
Beatriz <beatrizhilariop1@gmail.com>
2026-03-25 14:42:03 -0300

    Limpeza no código

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     const text = document.getElementById('glow-text');
-
-    // Verifica se o dispositivo possui um cursor de mouse real (Desktop)
-    // Isso evita que o efeito de hover fique "preso" em telas de toque (Mobile/Tablet)
     const isDesktop = window.matchMedia("(hover: hover) and (pointer: fine)").matches;
 
-    // Só ativa o efeito de luz se o texto existir E for um desktop
+    
     if(text && isDesktop) {
-        // Inicializa o CSS do texto para o gradiente dinâmico
         text.style.background = `radial-gradient(
             circle at 50% 50%, 
             #f82f2f 0%, 
@@ -20,7 +16,6 @@ document.addEventListener('DOMContentLoaded', () => {
         text.addEventListener('mousemove', (e) => {
             const rect = text.getBoundingClientRect();
             const x = ((e.clientX - rect.left) / rect.width) * 100;
-            // O "- 25" serve para alinhar o brilho verticalmente com o cursor
             const y = (((e.clientY - rect.top) / rect.height) * 100) - 25;
             
             text.style.background = `radial-gradient(
